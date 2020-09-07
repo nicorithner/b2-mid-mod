d8f50dbb7f02f6c03550c69746325778dcdbc891
Nico Rithner <nicorithner@gmail.com>
2020-09-07 16:11:46 -0600

    Set up show action and strong params in movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,11 @@
 class MoviesController < ApplicationController
+
+  def show
+    @movie = Movie.find(movie_params[:id])
+  end
+
+  private
+  def movie_params
+    params.permit(:id, :title, :creation_year, :genre)
+  end
 end
