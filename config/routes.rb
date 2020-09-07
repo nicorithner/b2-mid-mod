079f3367f70cf943dddebc5f8a50eac7b4d94de4
Nico Rithner <nicorithner@gmail.com>
2020-09-07 16:10:30 -0600

    Set up routes for movie show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-#=== Studios
-get '/studios', to: 'studios#index'
+  #=== Studios
+  get '/studios', to: 'studios#index'
+
+  #==== Movies
+  get '/movies/:id', to: 'movies#show'
+
 end
